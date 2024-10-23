2bb0115dcfda5b6449c2f4aa64415c3124fd498e
ROS-TARA <herodon6h6@gmail.com>
2024-10-23 21:37:00 +0545

    Update and rename README.md to README.md.js

---
+++ README.md.js
@@ -0,0 +1,24 @@
+{
+let inf=prompt("enter your name");
+console.log("hello",inf );
+
+let scr = prompt("enter your number to change in gpa");
+if(scr>=90 && scr<=100) {
+    console.log(inf,"Congratulation! you have score A+ ");
+}
+else if(scr >=80 && scr <=89) {
+    console.log(inf,"Congratulation! you have score A");
+}
+else if(scr >=70 && scr <= 79) {
+    console.log(inf,"Congratulation! you have score B+");
+}
+else if(scr >= 60 && scr <= 69) {
+    console.log(inf,"Congratulation! you have score B+");
+}
+else if(scr >= 50 && scr <= 59) {
+    console.log(inf,"Congratulation! you have score c+");
+}
+else if(scr >= 0 && scr <= 49) {
+    console.log(inf,"OOPS! you got N.G ");
+}
+}
